fcd3965ca2320b7326cf81b103b598caff02365f
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-03-14 20:43:02 -0300

    Criei uma classe chamada GameStateMachine Ela modulariza todo a parte de escolher qual cena vai ser carregada e deixa a logica toda nessa classe

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,37 +2,17 @@ import React from 'react'
 import logo from '../logo-getup.png'
 import './Game.css';
 import '../animate.css'
-import {WelcomeScreen, InstructionsScreen, PlayingScreen, GameWonScreen} from './GameScenes'
+import GameStateMachine from '../containers/GameStateMachine'
 
 
 export default React.createClass({
-	getInitialState(){
-		return { game: 'welcome' }
-	},
-
-	gameComponent: null,
-	numContainers: 11,
-
-	handleGameState(){
-		// Handling state machine
-		if(this.state.game === 'welcome')
-			this.setState({game: 'instructions'})
-		if(this.state.game === 'instructions')
-			this.setState({game: 'play'})
-		else if(this.state.game === 'play')
-			this.setState({game: 'won'})
-		else if(this.state.game === 'won')
-			this.setState({game: 'play'})
-	},
-
 	render(){
-		this.checkButtonOrGame()
 		return (
 			<div>
 				<div className='bgImage'>
 					<div className='title-box'>
 						<h1>I Dare you to kill my app</h1>
-						{this.gameComponent}
+						<GameStateMachine/>
 					</div>
 					<div className='logoFooter'>
 						<img src={logo} alt='Logo' className='img-logo'/>
@@ -40,27 +20,7 @@ export default React.createClass({
 				</div>
 			</div>
 			)
-	},
-
-	checkButtonOrGame(){
-		switch(this.state.game){
-			case 'welcome':
-				this.gameComponent = <WelcomeScreen onClick={this.handleGameState}/>
-				break
-			case 'instructions':
-				this.gameComponent = <InstructionsScreen onClick={this.handleGameState}/>
-				break
-			case 'play':
-				this.gameComponent = <PlayingScreen numContainers={this.numContainers} 
-										inCaseOfWin={this.handleGameState}/>
-				break
-			case 'won':
-				this.gameComponent = <GameWonScreen onClick={this.handleGameState}/>
-				break
-			default:
-				break
-		}
-
-
 	}
+
+	
 })
---
+++ src/containers/GameStateMachine.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {	WelcomeScreen, 
+			InstructionsScreen, 
+			PlayingScreen, 
+			GameWonScreen} from '../components/GameScenes'
+
+
+export default React.createClass({
+	getInitialState(){
+		return { game: 'welcome' }
+	},
+	gameScene: null,
+	numContainers: 11,
+
+	handleGameState(){
+		// Handling state machine
+		if(this.state.game === 'welcome')
+			this.setState({game: 'instructions'})
+		if(this.state.game === 'instructions')
+			this.setState({game: 'play'})
+		else if(this.state.game === 'play')
+			this.setState({game: 'won'})
+		else if(this.state.game === 'won')
+			this.setState({game: 'play'})
+	},
+
+	chooseState(){
+		switch(this.state.game){
+			case 'welcome':
+				this.gameScene = <WelcomeScreen onClick={this.handleGameState}/>
+				break
+			case 'instructions':
+				this.gameScene = <InstructionsScreen onClick={this.handleGameState}/>
+				break
+			case 'play':
+				this.gameScene = <PlayingScreen numContainers={this.numContainers} 
+										inCaseOfWin={this.handleGameState}/>
+				break
+			case 'won':
+				this.gameScene = <GameWonScreen onClick={this.handleGameState}/>
+				break
+			default:
+				break
+		}
+	},
+
+	render(){
+		this.chooseState()
+		return (<div>{this.gameScene}</div>)
+	}
+})
+
