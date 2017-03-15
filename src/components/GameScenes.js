a6a29afea93d1bba5f36a56a1df4d26ce38f1339
Danilo Aleixo <danilo_aleixo@hotmail.com>
2017-03-15 14:16:33 -0300

    Coloquei botões de compartilhar tanto no face como no twitter

--- src/components/GameScenes.js
+++ src/components/GameScenes.js
@@ -50,8 +50,34 @@ class GameWonScreen extends Component {
 	render(){
 		return (
 			<div className='animated fadeIn'>
-				<h2>You won</h2>
-				<button onClick={this.props.onClick}>Play Again</button>
+				<div className='instructions'>
+					<div className='textbox'>
+						<h2 className='title'>You won</h2>
+						<button onClick={this.props.onClick}>Play Again</button>
+						<h3 className='title'>
+							Share
+						</h3>
+						{/* Share face*/}
+						<span
+							data-href="https://daretokill-79f8f.firebaseapp.com/" 
+							data-layout="button" 
+							data-mobile-iframe="true"><a 
+							className="fb-xfbml-parse-ignore" 
+							target="_blank" 
+							href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fdaretokill-79f8f.firebaseapp.com%2F&amp;src=sdkpreparse">
+								<button className='share-button'>Facebook</button>
+							</a>
+						</span>
+					{/* Share twitter*/}
+						<span>
+							<a href="https://twitter.com/share" 
+								data-url="https://daretokill-79f8f.firebaseapp.com/" 
+								data-hashtags="getupcloud">
+									<button className='share-button'>Twitter</button>
+							</a> 
+						</span>
+					</div>
+				</div>
 			</div>
 		)
 	}
